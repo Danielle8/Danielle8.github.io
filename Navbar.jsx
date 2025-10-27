92ed6ddae0d27db9f081f0701e04b475acadffc0
Danielle8 <crevecoeurdanielle@gmail.com>
2025-10-27 14:49:42 -0500

    Create Navbar.jsx
    Adding navbar titles

---
+++ Navbar.jsx
@@ -0,0 +1,20 @@
+
+
+const Navbar () => {
+
+  return (
+    <div className ='navbar'>
+      <ul className="nav-menu">
+        <li>Danielle Crevecoeur</li>
+        <li>About Me</li>
+        <li>Projects</li>
+      </ul>
+      <div> className="nav-connect">Connect With Me</div>
+      
+    
+    
+    
+    
+    </div>
+
+}
