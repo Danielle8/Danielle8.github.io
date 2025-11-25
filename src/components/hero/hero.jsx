acff732b1e4c0150e472d5acec35029abeb0d14e
Danielle Crevecoeur <Crevecoeurdanielle@gmail.com>
2025-11-24 18:36:10 -0600

    removing profile img

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -4,7 +4,6 @@ import './hero.css'
 const Hero = () => {
     return (
         <div className='hero'>
-        {/* <img src={profile_img} alt="" /> */}
         <h1>Hi, my name is Danielle!</h1>
         <h2>Software Engineer and UI/UX Designer @ Dell</h2>
         <p>With 7 years in the CS field, spanning a high-ranking university and a leading tech company, I have thrived on high-impact, fast-paced projects across diverse teams.</p>
